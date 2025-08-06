85a2f965926e72541e37445b197d3776a3cb9ee0
samiraihann <raihannsamii11@gmail.com>
2025-08-06 13:50:53 +0700

    yyy

---
+++ project-nuxt/pages/program/[id].vue
@@ -0,0 +1,18 @@
+<!-- pages/program/[id].vue -->
+<template>
+  <div class="p-6">
+    <div v-if="pending">Memuat data...</div>
+    <div v-else-if="error">Program tidak ditemukan.</div>
+    
+    <div v-else>
+      <h1 class="text-2xl font-bold mb-2">{{ data.title }}</h1>
+      <p class="text-sm mb-4">{{ data.description }}</p>
+      <NuxtLink to="/program" class="text-blue-600 underline text-sm">← Kembali</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const { data, pending, error } = await useFetch(`http://localhost:3000/campaigns/${route.params.id}`)
+</script>
