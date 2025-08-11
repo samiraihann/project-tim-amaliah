b7ccd44c26aede2c504cba1c951ca5d0996f3df8
artha <arthaparulian31@gmail.com>
2025-08-11 11:29:10 +0700

    oihfidof

---
+++ project-nestjs/src/post.entity.ts
@@ -0,0 +1,14 @@
+// src/post/post.entity.ts
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  content: string;
+}
